8057a757e1b3eb062ad619f6772935f3b7cdb721
Deep Kothari <deepk2891@gmail.com>
2023-11-26 12:51:54 +0530

    Video-60 JAVASCRIPT Strings

---
+++ Video-60/index.js
@@ -0,0 +1,35 @@
+console.log('This is String')
+
+a = 'deep'
+
+console.log(a[ 0 ])
+console.log(a[ 1 ])
+console.log(a[ 2 ])
+console.log(a[ 3 ])
+// console.log(a[ 4 ])
+
+console.log(a.length)
+
+let real_name = 'deep'
+let friend = 'john'
+
+console.log("His name is " + real_name + " and his friend's is " + friend)
+
+console.log(`His name is ${ real_name } and his friend's name is ${ friend }`)
+
+
+let b = 'DeepKothari'
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.includes('s'))
+console.log(b.length)
+console.log(b.slice(1,5))
+console.log(b.slice(1))
+
+console.log(b.replace('sh','00'))
+console.log(b.concat(a,'FullStack','Engineer'))
+console.log(b.indexOf('h'))
+console.log(b.charAt(4))
+console.log(b.startsWith('D'))
+console.log(b.endsWith('i'))
+
