91a9b99cb3141875008c10bc9971b331e9a20b73
Deep Kothari <deepk2891@gmail.com>
2024-01-15 22:13:44 +0530

    Video-86 CommonJs Vs EcmaScript Modules

---
+++ Video-86/main.js
@@ -0,0 +1,14 @@
+// const http = require('node:http');
+
+// import http from "http"
+
+// import { a,b,c,d,e } from "./myModule"
+
+// console.log(a,b,c,d,e);
+
+// import obj from "./myModule"
+// console.log(obj);
+
+const a = require("./myModule2")
+
+console.log(a,__dirname,__filename)
